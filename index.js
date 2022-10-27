dc06f50a737c7d6d0acdadc8eb3b3516e70c17da
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-10-27 14:14:44 -0400

    done for now. Will return to add more complex styling

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-fetch("https://apis.scrimba.com/bored/api/activity")
-  .then((res) => res.json())
-  .then((data) => {
-    document.querySelector(".idea-description").textContent = data.activity;
-  });
+document.querySelector(".idea-btn").addEventListener("click", () => {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then((res) => res.json())
+    .then((data) => {
+      document.querySelector(".idea-description").textContent = data.activity;
+    });
+});
